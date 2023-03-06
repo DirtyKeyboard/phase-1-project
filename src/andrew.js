ec332d43a38a7fd23cc7507b97d86679abffd156
Andrew Hawileh <andrewh829@gmail.com>
2023-03-06 11:50:31 -0600

    button listnr

--- src/andrew.js
+++ src/andrew.js
@@ -1,1 +1,16 @@
-//DARK MODE
+const darkModeBtn = document.querySelector('button.fr')
+darkModeBtn.addEventListener('click', (e) =>
+{
+    if (darkModeBtn.textContent === 'Dynamic BG: Disabled')
+    {
+        console.log('Enable Dynamic BG')
+        darkModeBtn.textContent = 'Dynamic BG: Enabled'
+        //enable dark mode
+    }
+    else
+    {
+        console.log('Disabling Dynamic BG')
+        darkModeBtn.textContent = 'Dynamic BG: Disabled'
+        //enable light mode
+    }
+})
