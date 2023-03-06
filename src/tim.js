9fe052e29717ccc60b84e16d417e7132277789f2
Timothy <sanohoang@gmail.com>
2023-03-06 11:52:27 -0600

    changes to Tim's js

--- src/tim.js
+++ src/tim.js
@@ -1,3 +1,26 @@
 function fetchCars() {
-  fetch("http://localhost:3000/CARS");
+  fetch("http://localhost:3000/CARS")
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data.forEach((cars) => {
+        displayCars(cars);
+      });
+    });
+}
+
+fetchCars();
+
+function displayCars(cars) {
+  const carList = document.querySelector("h2");
+  const img = document.createElement("img");
+  img.src = cars.Img;
+  img.alt = cars.Model;
+  img.style.height = "125px";
+  img.style.width = "125px";
+  carList.append(img);
+
+  img.addEventListener("click", () => {});
 }
