6b76e2de331c82312a6d3fb6c2355583ce69e0b1
Andrew Hawileh <andrewh829@gmail.com>
2023-03-06 16:02:08 -0600

    added alot

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const show = document.getElementById("action-window");
+
 function fetchCars() {
   fetch("http://localhost:3000/CARS")
     .then((resp) => {
@@ -26,7 +28,6 @@ function displayCars(cars) {
   carList.append(img, name);
 
   img.addEventListener("click", () => {
-    const show = document.getElementById("action-window");
     show.innerText = "";
     const carImg = document.createElement("img");
     carImg.src = cars.img;
@@ -43,4 +44,83 @@ function displayCars(cars) {
 
     show.append(carImg, car, details, seller);
   });
+}
+
+const sellCarBtn = document.querySelector('button.fl')
+sellCarBtn.addEventListener('click', showForm)
+
+function showForm(e)
+{
+      show.innerHTML = "";
+      const form = document.createElement('form');
+      const h = document.createElement('h3')
+      const yearIn = document.createElement('input')
+      const makeIn = document.createElement('input')
+      const modelIn = document.createElement('input')
+      const imgIn = document.createElement('input')
+      const contactIn = document.createElement('input')
+      const detailsIn = document.createElement('input')
+
+      const labelYear = document.createElement('label')
+      const labelMake = document.createElement('label')
+      const labelModel = document.createElement('label')
+      const labelImg = document.createElement('label')
+      const labelContact = document.createElement('label')
+      const labelDetails = document.createElement('label')
+      const br = document.createElement('br')
+      const submitBtn = document.createElement('input')
+
+      labelYear.for = 'new-year'
+      labelMake.for = 'new-make'
+      labelModel.for = 'new-model'
+      labelImg.for = 'new-image'
+      labelContact.for = 'new-contact'
+      labelDetails.for = 'new-details'
+
+      labelYear.textContent = 'Year: ';
+      labelMake.textContent = 'Make: ';
+      labelModel.textContent = 'Model: ';
+      labelImg.textContent = 'Image: ';
+      labelContact.textContent = 'Contact Info: ';
+      labelDetails.textContent = 'Details: ';
+      submitBtn.textContent = 'Submit'
+
+      h.textContent = "Sell Your Car!"
+      yearIn.type = 'number'
+      makeIn.type = 'text'
+      modelIn.type = 'text'
+      imgIn.type = 'text'
+      contactIn.type = 'text'
+      detailsIn.type = 'text'
+      submitBtn.type = 'submit'
+
+      yearIn.classList.add('new-year')
+      makeIn.classList.add('new-make')
+      modelIn.classList.add('new-model')
+      imgIn.classList.add('new-image')
+      contactIn.classList.add('new-contact')
+      detailsIn.classList.add('new-details')
+      form.addEventListener('submit', (e) => {
+          e.preventDefault()
+          const newCarObject = {
+            //year, make, model, img, details, contact
+            year: yearIn.value,
+            make: makeIn.value,
+            model: modelIn.value,
+            img: imgIn.value,
+            details: detailsIn.value,
+            contact: contactIn.value,
+            sold: false
+          }
+          console.log(newCarObject)
+          show.innerHTML = "";
+          fetch('http://localhost:3000/CARS/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newCarObject)
+          }).then(() => displayCars(newCarObject))
+          form.reset();
+      })
+      form.append(labelYear, yearIn, labelMake, makeIn, labelModel, modelIn, labelImg, imgIn, labelContact, contactIn, br, labelDetails, detailsIn, submitBtn)
+      show.append(form)
 }
